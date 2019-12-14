6775fd5337079d3d35c520b3e4e5410dc5252f3d
chemric <11702279@student.pxl.be>
2019-12-14 13:09:52 +0100

    files opgekuist

--- WatchConnection/watchConnection2.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-#cd ~
-if [ -n "$1"]
-then
-	echo "ok"
-	-d -n 1 "/home/student/Desktop/WatchConnection/pingArpClients.sh"
-else
-	-d -n 1 "/home/student/Desktop/WatchConnection/pingArpClients.sh"
-fi
